ad07247c38f61e06a910665a8048badf4f33585f
UmairAhmedImran <umairahmedimranbutt@gmail.com>
2023-09-02 19:28:33 +0500

    Creating a concatenation function file

---
+++ concatenationFunction.js
@@ -0,0 +1,10 @@
+// create a function to greet 
+let name = "Umair"
+let greetings = "Hi there"
+
+function greet() {
+    let statement = greetings + ", " + name + "!"
+    console.log(statement)
+}
+
+greet()
--- variablesPractice.js
+++ variablesPractice.js
@@ -1,3 +1,4 @@
+// create two variables, concatenate them and log them out
 let firstName = "Umair Ahmed"
 let lastName = "Imran Butt"
 let fullName = firstName + " " +lastName
