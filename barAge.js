29d1ab2c5c3b35123bc45fa101c160816ac0e7ba
UmairAhmedImran <umairahmedimranbutt@gmail.com>
2023-09-03 18:39:12 +0500

    If else problem of the bar age

---
+++ barAge.js
@@ -0,0 +1,13 @@
+// Check if the person is old enough to enter the nightclub (21)
+// Log a suitable message to the console in both cases
+
+let age = 22
+
+// if less than 21 -> "You can not enter the club!"
+// else            -> "Welcome!"
+
+if (age > 21) {
+    console.log("You can not enter the club!")
+} else {
+    console.log("Welcome")
+}
