44837d0dfc2f490c6945957e0c2f78b8384604dc
UmairAhmedImran <umairahmedimranbutt@gmail.com>
2023-09-03 23:04:34 +0500

    understanding booleans

---
+++ trueFalse.js
@@ -0,0 +1,9 @@
+// tell if the statement is true or false
+
+console.log(4 === 3)  // false
+console.log(5 > 2)    // true
+console.log(12 > 12)  // false
+console.log(3 < 0)    // false
+console.log(3 >= 3)   // true
+console.log(11 <= 11) // true
+console.log(3 <= 2)   // false
